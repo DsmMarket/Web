3acd587d6ee50c3632fa6a80227097c64fc490ea
RRelease <jayhadas@naver.com>
2019-09-16 23:48:13 +0900

    mainPage수정

--- mainPage/js/mainPage.js
+++ mainPage/js/mainPage.js
@@ -9,3 +9,40 @@ window.addEventListener('load', () => {
         document.body.style.backgroundImage = 'url("https://source.unsplash.com/1600x900/?morning,city")';
     }
 })
+
+
+const mainPage_itemCarousel = document.querySelector('.mainPage_itemCarousel');
+
+function carouselChangedLeft() {
+    mainPage_itemCarousel.classList.remove('carouselShopChangedRight');
+    mainPage_itemCarousel.classList.remove('carouselRentChangedRight');
+    mainPage_itemCarousel.classList.remove('carouselShopChangedLeft');
+    mainPage_itemCarousel.classList.remove('carouselRentChangedLeft');
+    if(mainPage_itemCarousel.classList.contains('carouselRent')) {
+        mainPage_itemCarousel.classList.add('carouselRentChangedLeft');
+        mainPage_itemCarousel.classList.remove('carouselRent');
+    } else {
+        mainPage_itemCarousel.classList.add('carouselShopChangedLeft');
+        mainPage_itemCarousel.classList.add('carouselRent');
+    }
+}
+
+function carouselChangedRight() {
+    mainPage_itemCarousel.classList.remove('carouselShopChangedLeft');
+    mainPage_itemCarousel.classList.remove('carouselRentChangedLeft');
+    mainPage_itemCarousel.classList.remove('carouselShopChangedRight');
+    mainPage_itemCarousel.classList.remove('carouselRentChangedRight');
+    if(mainPage_itemCarousel.classList.contains('carouselRent')) {
+        mainPage_itemCarousel.classList.add('carouselRentChangedRight');
+        mainPage_itemCarousel.classList.remove('carouselRent');
+    } else {
+        mainPage_itemCarousel.classList.add('carouselShopChangedRight');
+        mainPage_itemCarousel.classList.add('carouselRent');
+    }
+}
+
+
+
+
+
+
