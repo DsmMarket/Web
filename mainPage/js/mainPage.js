d8aea7db1d7014024e0c98f672570691bb49b003
RRelease <jayhadas@naver.com>
2019-09-30 08:15:31 +0900

    개별 상품 개발중

--- mainPage/js/mainPage.js
+++ mainPage/js/mainPage.js
@@ -1,6 +1,8 @@
 window.addEventListener('load', () => {
     isUserLogin();
     setBackground();
+    getDealList();
+    getRentList();
 })
 
 function isUserLogin() {
@@ -31,7 +33,7 @@ function isUserLogin() {
 }
 
 function getNickName() {
-    axios.get(`http://52.78.148.203//user/nick`, {
+    axios.get(`http://52.78.148.203/user/nick`, {
         headers: {
             "Authorization" : localStorage.getItem("accessToken"),
         }
@@ -65,7 +67,9 @@ function changeLoginOut(v) {
 
 function reissuanceToken() {
     axios.get(`http://52.78.148.203/token`, {
-        "refresh_token": localStorage.getItem("refreshToken"),
+        headers: {
+            "Authorization": localStorage.getItem("refreshToken"),
+        }
     })
     .then((response) => {
         if(response.status === 200) {
@@ -104,6 +108,100 @@ function setBackground() {
 }
 
 
+const mainPageItemCarouselShop = document.getElementById('mainPageItemCarouselShop');
+const mainPageItemCarouselRent = document.getElementById('mainPageItemCarouselRent');
+let title;
+function fillItemCarousel(dR, data) {
+    if(dR == 'deal') {
+        data.forEach((v) => {
+            if(v.title.length >= 9){title = v.title.substr(0, 9) + "..."}else{title = v.title}
+            mainPageItemCarouselShop.innerHTML += `
+            <div class="mainPage_itemCarousel_content" onclick="itemClicked(${v.postId}, 0)">
+                <div class="mainPage_itemCarousel_content_image"><img src="${v.img}"></div>
+                <div class="mainPage_itemCarousel_content_namePrice">
+                    <h6 class="mainPage_itemCarousel_content_name">${title}</h6>
+                    <p class="mainPage_itemCarousel_content_price">${v.price}</p>
+                </div>
+            </div>`
+        });
+        popupOpen();
+    } else if(dR == 'rent') {
+        data.forEach((v) => {
+            if(v.title.length >= 9){title = v.title.substr(0, 9) + "...";}else{title = v.title;}
+            mainPageItemCarouselRent.innerHTML += `
+            <div class="mainPage_itemCarousel_content" onclick="itemClicked(${v.postId}, 1)">
+                <div class="mainPage_itemCarousel_content_image"><img src="${v.img}"></div>
+                <div class="mainPage_itemCarousel_content_namePrice">
+                    <h6 class="mainPage_itemCarousel_content_name">${title}</h6>
+                    <p class="mainPage_itemCarousel_content_price">${v.price}</p>
+                </div>
+            </div>`
+        });
+    } else {
+        console.log('deal or rent fail');
+    }
+}
+
+function getDealList() {
+    axios.get(`http://52.78.148.203/list/deal`, {
+        params: {
+            "page": 1,
+            "pagesize": 4,
+            "search": "",
+            "category": "",
+        },
+        headers: {
+            "Authorization": localStorage.getItem("accessToken"),
+        }
+    })
+    .then((response) => {
+        if(response.status === 200) {
+            console.log('9. refer success(Deal)');
+            fillItemCarousel('deal', response.data.list);
+        } else if(response.status === 401) {
+            console.log('3. invalid access token(Deal)');
+
+        } else {
+            console.log(`Error: status code[${response.status}]`);
+
+        }
+    })
+    .catch((reject) => {
+        console.log("상품 조회에 실패하셨습니다." + reject + " and " + reject.response);
+
+    })
+}
+function getRentList() {
+    axios.get(`http://52.78.148.203/list/rent`, {
+        params: {
+            "page": 1,
+            "pagesize": 4,
+            "search": "",
+            "category": "",
+        },
+        headers: {
+            "Authorization": localStorage.getItem("accessToken"),
+        }
+    })
+    .then((response) => {
+        if(response.status === 200) {
+            console.log('9. refer success(rent)');
+            fillItemCarousel('rent', response.data.list);
+        } else if(response.status === 401) {
+            console.log('3. invalid access token(rent)');
+
+        } else {
+            console.log(`Error: status code[${response.status}]`);
+
+        }
+    })
+    .catch((reject) => {
+        console.log("대여 조회에 실패하셨습니다." + reject + " and " + reject.response);
+
+    })
+}
+
+
 const mainPage_itemCarousel = document.querySelector('.mainPage_itemCarousel');
 
 function carouselChangedLeft() {
@@ -133,6 +231,73 @@ function carouselChangedRight() {
         mainPage_itemCarousel.classList.add('carouselRent');
     }
 }
+
+const popup_showItemImg = document.getElementById('popup_showItemImg');
+const popup_showItemImgs = document.getElementById('popup_showItemImgs');
+
+function fillItemModal(data, dealRent) {
+    while (popup_showItemImgs.hasChildNodes()) {
+        popup_showItemImgs.removeChild(popup_showItemImgs.firstChild);
+    }
+    data.img.forEach((v, i) => {
+        popup_showItemImgs.innerHTML += `
+        <li>
+            <img src="${v}">
+        </li>`
+    });
+    popup_showItemImg.setAttribute('src', data.img[0]);
+}
+
+function itemClicked(postId, type) {
+    axios.get(`http://52.78.148.203/post`, {
+        params: {
+            "postId": postId,
+            "type": type,
+        },
+        headers: {
+            "Authorization": localStorage.getItem("accessToken"),
+        }
+    })
+    .then((response) => {
+        if(response.status === 200) {
+            console.log('itemClickedSuccess');
+            fillItemModal(response.data, type);
+        } else if(response.status === 410) {
+            console.log('itemClickedFail');
+
+        } else {
+            console.log(`Error: status code[${response.status}]`);
+
+        }
+    })
+    .catch((reject) => {
+        console.log("개별 상품 조회에 실패하셨습니다." + reject + " and " + reject.response);
+
+    })
+}
+
+const popup_showItem = document.getElementById("popup_showItem");
+
+function popupOpen() {
+    const mainPage_itemCarousel_content = document.querySelectorAll(".mainPage_itemCarousel_content");
+    mainPage_itemCarousel_content.forEach((e) => {
+        e.addEventListener('click', () => {
+            popup_showItem.classList.remove("hidden");
+        })
+    })
+}
+
+popup_showItem.addEventListener('click', () => {
+    popup_showItem.classList.add("hidden");
+})
+
+document.getElementById("popup_showItemBox").addEventListener('click', () => {
+    event.stopPropagation();
+})
+
+
+
+
 
 
 
